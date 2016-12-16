48b0ab8da64514ff759be7c8c1afb28eba3328e5
Viraj Padte <virajpadte@gmail.com>
2016-12-16 13:43:11 -0700

    Added power measurement Arduino code

---
+++ getpower/getpower.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+Adafruit_INA219 ina219;
+
+void setup(void) 
+{
+  uint32_t currentFrequency;
+    
+  Serial.begin(115200);
+  ina219.begin();
+}
+
+void loop(void) 
+{
+  float current_mA = 0;
+  current_mA = ina219.getCurrent_mA();
+  
+  // Average Power
+  float power_W = current_mA * 19/1000;
+  Serial.println(power_W);
+  //delay(2000);
+}
